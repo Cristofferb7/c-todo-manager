56147ae9ec859fa9eed0efb2c39f40297356e408
Cristoffer Bohorquez <cristoffersmacbook@Cristoffers-MacBook-Air.local>
2025-09-06 16:30:33 -0400

    polished some of the logic

--- task-manager.c
+++ task-manager.c
@@ -2,17 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int status;
+
 void addTask(Task tasks[], int *taskCount)
 {
+int status;
 printf("please enter task\n");
 fgets(tasks[*taskCount].description,MAX_DESC_LENGTH,stdin);
 
 printf("is it completed?\n");
 scanf("%d", &status);
-(tasks[*taskCount].completed = status);
+tasks[*taskCount].completed = status;
 
+(*taskCount)++;
 
+printf("task added\n");
 
 }
 
